60907a375b584139e0dac49a405838a8b42d11a6
Andrew <narflz@gmail.com>
2021-06-29 16:24:05 -0500

    still not working

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -113,8 +113,9 @@ public class EventController {
         Event event = result.get();
         model.addAttribute("title", "Add Tag to: " + event.getName());
         model.addAttribute("tags", tagRepository.findAll());
-        model.addAttribute("event", event);
-        model.addAttribute("eventTag", new EventTagDTO());
+        EventTagDTO eventTag = new EventTagDTO();
+        eventTag.setEvent(event);
+        model.addAttribute("eventTag", eventTag);
         return "event/add-tag.html";
     }
 
